c7768d9403ab58e0b7ee2e83fc860a3eaa40e674
Adewale17 <degbaubjames@gmail.com>
2024-12-09 11:52:35 -0800

    run migration

--- database/migrations/2024_12_09_194245_create_tasks_table.php
+++ database/migrations/2024_12_09_194245_create_tasks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->unsignBigInteger('userid');
+            $table->foreignID('userid');
             $table->string('title');
             $table->string('description')->nullable();
             $table->date('day_from');
